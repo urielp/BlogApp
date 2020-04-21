d18d87831ef85ffc63674e71473c98c694721d31
UrielParienti <urielp@wix.com>
2020-04-21 12:21:20 +0300

    cleaning some imprts and move copmonents to SA views

--- BlogAppV2/src/components/posts/postForm/newPost.tsx
+++ BlogAppV2/src/components/posts/postForm/newPost.tsx
@@ -12,6 +12,7 @@ import CustomInput from "../../CustomInput/CustomInput";
 // import Button from '../CustomButtons/Button';
 import withStyles from "@material-ui/core/styles/withStyles";
 import createStyles from "@material-ui/core/styles/createStyles";
+
 import InputAdornment from "@material-ui/core/InputAdornment";
 import rtlStyle from "../../../assets/jss/material-dashboard-react/views/rtlStyle";
 import { container } from "../../../assets/jss/material-dashboard-react";
@@ -21,7 +22,7 @@ import Snackbar from "../../Snackbar/Snackbar";
 import SnackbarContent from "../../Snackbar/SnackbarContent";
 import Warning from "@material-ui/icons/Warning";
 import { SnackbarProvider, VariantType, useSnackbar } from "notistack";
-
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 const styles = createStyles({
   ...rtlStyle,
   container: {
@@ -31,9 +32,14 @@ const styles = createStyles({
     paddingTop: "20vh",
     color: "#FFFFFF",
     paddingBottom: "200px"
+  },
+  center: {
+    right: "25%"
   }
 });
-
+const theme = createMuiTheme({
+  direction: "rtl" // Both here and <body dir="rtl">
+});
 const newPostForm = (props: any) => {
   const { classes } = props;
   const [AsyncState, AsyncDispatch] = useStoreAsyncVersion(true);
@@ -44,8 +50,11 @@ const newPostForm = (props: any) => {
     console.log(message);
     return (
       <Snackbar
-        place="bc"
+        place="tc"
         color="warning"
+        classes={{
+          root: classes.root // class name, e.g. `classes-nesting-root-x`
+        }}
         icon={Warning}
         message={
           `${message.title ? message.title : ""}
@@ -58,124 +67,145 @@ const newPostForm = (props: any) => {
       />
     );
   };
-
+  const StyledTextField = withStyles({
+    root: {
+      "& label": {
+        transformOrigin: "top right",
+        right: 0,
+        left: "auto"
+      },
+      "& outline": {
+        transformOrigin: "top right",
+        right: 0,
+        left: "auto"
+      }
+    }
+  })(TextField);
   return (
-    <div>
-      <Formik
-        initialValues={{
-          author: "NA",
-          title: "",
-          subtitle: "",
-          body: ""
-        }}
-        validateOnChange={false}
-        validateOnBlur={false}
-        validate={values => {
-          const errors: any = {};
-          if (!values.title) {
-            //setOpen(true);
-            errors.title = "כותרת לא יכולה להיות ריקה";
-          }
-          if (!values.subtitle) {
-            //setOpen(true);
-            errors.subtitle = "\nחובה להזין תיאור קצר";
-          }
-          if (!values.body) {
-            //setOpen(true);
-            errors.body = "פוסט לא יכול להיות ריק";
-          }
-          if (errors.body || errors.title || errors.subtitle) {
-            setOpen(true);
-          }
-          return errors;
-        }}
-        onSubmit={(values, { setSubmitting }) => {
-          AsyncDispatch("NEW_POST", values);
-          props.history.push("/adminx/posts");
-        }}
-      >
-        {({
-          values,
-          errors,
-          touched,
-          handleChange,
-          handleBlur,
-          handleSubmit,
-          isSubmitting
-        }) => (
-          <GridContainer justify="center">
-            <GridItem xs={12} sm={12} md={12}>
-              <Card className={classes.textCenter}>
-                <CardHeader color="danger">פוסט חדש{isOpen}</CardHeader>
-                <CardBody>
-                  <form
-                    className={classes.form}
-                    role="form"
-                    onSubmit={handleSubmit}
-                  >
-                    <GridContainer>
-                      <GridItem xs={12} sm={12} md={9}>
-                        <CustomInput
-                          id="title"
-                          fullwidth
-                          inputProps={{
-                            placeholder: "כותרת לפוסט",
-                            variant: "outlined"
-                          }}
-                          formControlProps={{
-                            fullWidth: true,
-                            value: values.title,
-                            onChange: handleChange
-                          }}
-                        />
-                        {/* {errors.title ? handleClose(errors.title) : null} */}
-                      </GridItem>
-                      <GridItem xs={12} sm={12} md={9}>
-                        <CustomInput
-                          id="subtitle"
-                          inputProps={{
-                            placeholder: "תיאור קצר",
-                            variant: "outlined"
-                          }}
-                          formControlProps={{
-                            fullWidth: true,
-                            value: values.subtitle,
-                            onChange: handleChange
-                          }}
-                        />
-                        {/* {errors.subtitle ? handleClose(errors.subtitle) : null} */}
-                      </GridItem>
-                      <GridItem xs={12} sm={12} md={9}>
-                        <CustomInput
-                          id="body"
-                          outlined
-                          inputProps={{
-                            placeholder: "תוכן",
-                            variant: "outlined"
-                          }}
-                          formControlProps={{
-                            fullWidth: true,
-                            value: values.body,
-                            onChange: handleChange
-                          }}
-                        />
-                        {errors.body || errors.title || errors.subtitle
-                          ? handleClose(errors)
-                          : null}
-                      </GridItem>
-                    </GridContainer>
-                    <Button color="primary" type="submit">
-                      שמור
-                    </Button>
-                  </form>
-                </CardBody>
-                {/* <CardFooter className={classes.textMuted}>2 days ago</CardFooter> */}
-              </Card>
-            </GridItem>
-          </GridContainer>
-        )}
-      </Formik>
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <div dir="rtl">
+        <GridContainer justify="center">
+          <GridItem xs={10} sm={10} md={10}>
+            <Formik
+              initialValues={{
+                author: "NA",
+                title: "",
+                subtitle: "",
+                body: ""
+              }}
+              validateOnChange={false}
+              validateOnBlur={false}
+              validate={values => {
+                const errors: any = {};
+                if (!values.title) {
+                  //setOpen(true);
+                  errors.title = "כותרת לא יכולה להיות ריקה";
+                }
+                if (!values.subtitle) {
+                  //setOpen(true);
+                  errors.subtitle = "\nחובה להזין תיאור קצר";
+                }
+                if (!values.body) {
+                  //setOpen(true);
+                  errors.body = "פוסט לא יכול להיות ריק";
+                }
+                if (errors.body || errors.title || errors.subtitle) {
+                  setOpen(true);
+                }
+                return errors;
+              }}
+              onSubmit={(values, { setSubmitting }) => {
+                AsyncDispatch("NEW_POST", values);
+                props.history.push("/adminx/posts");
+              }}
+            >
+              {({
+                values,
+                errors,
+                touched,
+                handleChange,
+                handleBlur,
+                handleSubmit,
+                isSubmitting
+              }) => (
+                <GridContainer justify="center">
+                  <GridItem xs={12} sm={12} md={12}>
+                    <Card className={classes.textCenter}>
+                      <CardHeader color="danger">פוסט חדש{isOpen}</CardHeader>
+                      <CardBody>
+                        <form
+                          className={classes.form}
+                          role="form"
+                          onSubmit={handleSubmit}
+                        >
+                          <GridContainer>
+                            <GridItem xs={12} sm={12} md={9}>
+                              <CustomInput
+                                id="title"
+                                fullwidth
+                                inputProps={{
+                                  placeholder: "כותרת לפוסט",
+                                  variant: "outlined"
+                                }}
+                                formControlProps={{
+                                  fullWidth: true,
+                                  value: values.title,
+                                  onChange: handleChange
+                                }}
+                              />
+                              {/* {errors.title ? handleClose(errors.title) : null} */}
+                            </GridItem>
+                            <GridItem xs={12} sm={12} md={9}>
+                              <CustomInput
+                                id="subtitle"
+                                inputProps={{
+                                  placeholder: "תיאור קצר",
+                                  variant: "outlined"
+                                }}
+                                formControlProps={{
+                                  fullWidth: true,
+                                  value: values.subtitle,
+                                  onChange: handleChange
+                                }}
+                              />
+                              {/* {errors.subtitle ? handleClose(errors.subtitle) : null} */}
+                            </GridItem>
+                            <GridItem xs={12} sm={12} md={9}>
+                              <CustomInput
+                                id="body"
+                                outlined
+                                inputProps={{
+                                  placeholder: "תוכן",
+                                  variant: "outlined",
+                                  multiline: true,
+                                  rows: 5
+                                }}
+                                formControlProps={{
+                                  fullWidth: true,
+                                  value: values.body,
+                                  onChange: handleChange
+                                }}
+                              />
+                              {errors.body || errors.title || errors.subtitle
+                                ? handleClose(errors)
+                                : null}
+                            </GridItem>
+                          </GridContainer>
+                          <Button color="primary" type="submit">
+                            שמור
+                          </Button>
+                        </form>
+                      </CardBody>
+                      {/* <CardFooter className={classes.textMuted}>2 days ago</CardFooter> */}
+                    </Card>
+                  </GridItem>
+                </GridContainer>
+              )}
+            </Formik>
+          </GridItem>
+        </GridContainer>
+      </div>
+    </MuiThemeProvider>
   );
 };
 export default withStyles(styles)(newPostForm);
---
+++ BlogAppV2/src/views/LoginPage/loginPage.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import Login from "../../components/LoginX/LoginX";
+
+export const LoginPage = (props: any) => {
+  return <Login />;
+};
